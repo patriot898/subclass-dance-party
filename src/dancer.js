45c33818a562ead3b9fc0d99393f99c542103d3b
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-03-31 11:10:59 -0700

    Refactor Dancer Class to Pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -1,24 +1,31 @@
 // Creates and returns a new dancer object that can step
 var MakeDancer = function(top, left, timeBetweenSteps) {
-
+  // var this = Object.create(MakeDancer.prototype)
+  //this.step();
+  this.timeBetweenSteps = timeBetweenSteps;
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
+  console.log(this);
 
   this.step();
+  //this.timeBetweenSteps = timeBetweenSteps;
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
 
+  // return this
 
 };
 
 MakeDancer.prototype.step = function () {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
+
+//setTimeout(function () {brushTeeth.call(alice)})
 
 MakeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
@@ -30,3 +37,4 @@ MakeDancer.prototype.setPosition = function(top, left) {
   };
   this.$node.css(styleSettings);
 };
+
